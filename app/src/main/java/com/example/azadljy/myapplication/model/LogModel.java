24bbce21f843c3fef4bf207045a063ef35dc2627
Ljy2016 <enjoy_azad@sina.com>
2017-02-28 18:03:51 +0800

    加入recyclerView以及databinding

---
+++ app/src/main/java/com/example/azadljy/myapplication/model/LogModel.java
@@ -0,0 +1,29 @@
+package com.example.azadljy.myapplication.model;
+
+/**
+ * 作者：Ljy on 2017/2/28.
+ * 邮箱：enjoy_azad@sina.com
+ */
+
+public class LogModel {
+    private String logTime;
+    private String logContent;
+
+    public String getLogContent() {
+        return logContent;
+    }
+
+    public void setLogContent(String logContent) {
+        this.logContent = logContent;
+    }
+
+    public String getLogTime() {
+        return logTime;
+    }
+
+    public void setLogTime(String logTime) {
+        this.logTime = logTime;
+    }
+
+
+}
